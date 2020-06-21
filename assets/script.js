ca1494718d0a7ec50f4441c9f77e74e21188bfc6
Vox Humana <jonprine@gmail.com>
2020-06-21 13:11:20 -0400

    fixed lowercase and uppercase randomize issues

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,8 @@
 var generateBtn = document.querySelector("#generate");
 // create object and place all characters as values
 var passChar = {
-  lower: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z",
-  upper: "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z",
+  lower: "qwertyuiopasdfghjklzxcvbnm",
+  upper: "QWERTYUIOPASDFGHJKLZXCVBNM",
   numerical: "0,1,2,3,4,5,6,7,8,9",
   symbol: "!@#$%^&*(){}[]=<>/,.",
 };
@@ -51,6 +51,12 @@ function generatePassword() {
     if (symbol === true) {
         passwordCheck += passChar.symbol;
     };
+
+    var password = "";
+    for (let i = 0; i < charAmount; i++) {
+      password += passwordCheck[Math.floor(Math.random() * passwordCheck.length)]
+    };
+    return password;
 };
 
 
